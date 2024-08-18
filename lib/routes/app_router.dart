df8620d3d5ac4114b53eb6cb80472b079fa80e3e
Arturo Alejandro Aragón <aragondev@Arturos-MacBook-Pro.local>
2024-08-18 02:57:20 -0400

    install plugins dio, riverpod and gorouter

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,19 +1,1 @@
-import 'package:go_router/go_router.dart';
-import 'package:your_project_name/features/movie/presentation/pages/movie_list_page.dart';
-import 'package:your_project_name/features/movie/presentation/pages/movie_detail_page.dart';
 
-final appRouter = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => MovieListPage(),
-    ),
-    GoRoute(
-      path: '/movie/:id',
-      builder: (context, state) {
-        final id = state.params['id']!;
-        return MovieDetailPage(movieId: id);
-      },
-    ),
-  ],
-);
